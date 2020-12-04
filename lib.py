730f93e4945ddfcd7d7caa845fa1ae1363164d3b
Vumba798 <alexandrov32649@gmail.com>
2020-12-04 18:08:56 +0300

    delted vim files

--- lib.py
+++ lib.py
@@ -8,6 +8,7 @@ class Point:
         print("Y =", self.y)
         print()
 class NeuralNetwork:
+    tau = 1.618034
     def __init__(self, c, d, a, b, N):
         self.a = a
         self.b = b
@@ -21,9 +22,29 @@ class NeuralNetwork:
                  / (self.N+1)*(i+1) + self.a)
             tmpY = tmpX*self.c+self.d+uniform(-0.5,0.5)
             self.xArr.append(Point(tmpX, tmpY))
+    def f(self,  float: x):
+        return c*x + d
     def print_experiments(self):
         for elem in self.xArr:
             elem.print()
+    def sum_of_squares(self, float: c, float: d):
+        sum = 0
+        for i in range(self.N):
+            sum += f(xArr[i])
+        return sum
+    def create_coef_list(self, float: epsilon,
+                        float: c_min, float: c_max):
+        left = c_min
+        right = c_max
+        while right - left > epsilon:
+            c_right = (right - left) / tau + left;
+            c_left = (rihgt - left) / tau - right
+            if sum_of_squares(c_left) < c_right):
+                right = c_right
+            else:
+                left = c_left
+
+             
 
 nn = NeuralNetwork(3, 1, -2, 2, 3)
 nn.print_experiments()
